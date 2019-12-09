3be6581726782012a52226964e8b0daf97941b86
sbentley2019 <sbentley000@gmail.com>
2019-12-09 23:32:48 +0000

    Implement exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,3 +3,10 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: 'Las Vegas')
+
+puts @store1.name
